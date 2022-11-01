3f349e696ffe578d679f368069dc9db5f2bc1b99
Your Name <you@example.com>
2022-11-01 15:51:41 +0700

    sdasd

---
+++ IntrosdutionToJava/src/Khaibao.java
@@ -0,0 +1,19 @@
+public class Khaibao {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 30.4f;
+        double d = 23.56;
+        boolean b = true;
+        char a = 'i';
+        String s = "Ishikawa";
+        System.out.println(" i = " + i);
+        System.out.println(" f = " + f);
+        System.out.println(" d = " + d);
+        System.out.println(" b = " + b);
+        System.out.println(" a = " + a);
+        System.out.println(" s = " + s);
+
+    }
+
+
+}
---
+++ IntrosdutionToJava/src/ToanTu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ToanTu {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+
+        float Area = width*height;
+        System.out.println("Are is = " + Area);
+
+    }
+}
