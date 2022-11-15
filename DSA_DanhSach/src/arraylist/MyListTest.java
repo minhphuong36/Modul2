d1319529c06fe1241f5601bc7b2b2cde236b737e
Your Name <you@example.com>
2022-11-15 20:30:45 +0700

    sss

--- DSA_DanhSach/src/arraylist/MyListTest.java
+++ DSA_DanhSach/src/arraylist/MyListTest.java
@@ -1,16 +1,30 @@
-package arraylist;
-
-import org.testng.annotations.Test;
-
-public class MyListTest {
-    @Test
-    public void addTest(){
-        MyList list = new MyList();
-        list.add(0,1);
-        list.add(1,3);
-        list.add(2,5);
-        System.out.println("list :"+list);
-    }
-
-
-}
+//package arraylist;
+//
+//import org.testng.annotations.Test;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MyListTest<E> {
+//    @Test
+//    public void addTest(){
+//        MyList list = new MyList();
+//        list.add(0,1);
+//        list.add(1,3);
+//        list.add(2,5);
+//        System.out.println("list :"+list);
+//    }
+//
+//    public static void main(String[] args) {
+//
+//       final List<E> list = new MyList<>();
+//
+//            list.add(0,1);
+//            list.add(1,3);
+//            list.add(2,5);
+//            list.add(1,10);
+//            System.out.println("list :"+list);
+//    }
+//
+//
+//}
--- DSA_DanhSach/src/test/TestStatic.java
+++ DSA_DanhSach/src/test/TestStatic.java
@@ -1,7 +1,17 @@
 package test;
 
+import java.util.Arrays;
+
 public class TestStatic {
     public static void main(String[] args) {
-        System.out.println(StaticClass.testStatic);
+      String a = "hello các bạn, mình là phương. Mình là lập trình viên";
+      String s1 = "Mai Minh Phương";
+      String[] c = s1.split(" ");
+        System.out.println(Arrays.toString(c));
+        System.out.println("Tên: " + c[c.length-1]);
+
+
+//      String[] b = a.split("\\.|\\,| ");
+//        System.out.println(Arrays.toString(b));
     }
 }
